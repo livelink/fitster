36ed44587619320597f4e55ce5ac40022cba8f48
Flávio M. Oota <f@oota.com.br>
2022-01-25 13:10:14 +0000

    Setting rough to half of box width

--- src/defaults/aspect-matching-wrapper.js
+++ src/defaults/aspect-matching-wrapper.js
@@ -1,8 +1,6 @@
 export default class AspectMatchingWrapper {
-  constructor(box, { rough = 100, precision = 1 } = {}) {
+  constructor(box) {
     this._box = box;
-    this._rough = rough;
-    this._precision = precision;
     box.wrap = true;
   }
 
@@ -12,23 +10,24 @@ export default class AspectMatchingWrapper {
     this._adjustWidthWhile({
       condition: () =>
         this._box.frame.width > width && this._box.outer.aspect > aspect,
-      step: -this._rough,
+      step: -width / 2,
     });
 
     this._adjustWidthWhile({
       condition: () => this._box.outer.aspect < aspect,
-      step: this._precision,
+      step: 1,
     });
-    
+
     const frameWidth = this._box.frame.width;
 
     this._box.width = frameWidth <= width ? width : frameWidth;
     this._box.height = frameWidth / aspect;
   }
 
-  _adjustWidthWhile({ condition, step = 1 }) {
+  _adjustWidthWhile({ condition, step }) {
     while (condition()) {
       this._box.width = this._box.frame.width + step;
+
       if (!this._box.widthHasOverflow) continue;
 
       this._box.width = this._box.content.width;
